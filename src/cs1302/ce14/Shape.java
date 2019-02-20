93ab756389880c727263cd1f6b7fe01cfb3dafc3
Brad Barnes <bjb211@uga.edu>
2019-02-20 13:09:29 -0500

    Revert "Made checknull generic"
    This reverts commit 966eb7cc6c46ae65d8682e72f937b75c8490abe5.

--- src/cs1302/ce14/Shape.java
+++ src/cs1302/ce14/Shape.java
@@ -19,7 +19,7 @@ public abstract class Shape {
      * @throws IllegalArgumentException if {@code name} is an empty string
      */
     protected void setName(String name) {
-	Utility.<Object>checkNull("setName", new String[] {name});
+	Utility.checkNull("setName", new Object[] {name});
 	if (name.isEmpty()) {
 	    throw new IllegalArgumentException("name cannot be an empty string");
 	} else {
