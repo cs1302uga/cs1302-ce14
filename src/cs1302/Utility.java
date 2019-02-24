0850364251f57b65ea3db55aec32608d94ae80cf
Michael Cotterell (Supa' Mike) <mepcotterell@gmail.com>
2019-02-24 18:36:41 -0500

    updated exercise description

--- src/cs1302/Utility.java
+++ src/cs1302/Utility.java
@@ -7,20 +7,22 @@ package cs1302;
 public class Utility {
 
     /**
-     * Throws a {@code NullPointerException} if the value of the
-     * parameter, {@code o} is {@code null}. The method performs no 
-     * actions if {@code o} is not {@code null}.
+     * Throws a {@code NullPointerException} if the value of any
+     * references in {@code o} is {@code null}. The method performs no 
+     * actions if none of the references are {@code null}.
      *
      * @param method the name of the calling method.
      * @param o parameter containing the object to verify.
      * @throws NullPointerException if parameter {@code o} 
      * is {@code null}.
      */
-    public static void checkNull(String method, Object o) {
-	if(o == null) {
-	    String message = method + ": Null Argument Provided";
-	    throw new NullPointerException(message);
-	} // if
+    public static void checkNull(String method, Object ... o) {
+	for (Object obj : o) {
+	    if(obj == null) {
+		String message = method + ": Null Argument Provided";
+		throw new NullPointerException(message);
+	    } // if
+	} // for
     } // checkNull
 
 } // Utility
