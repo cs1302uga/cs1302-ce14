36d11855926718d90640c0c03d19ece4b9c9f3ea
bjb211 <bjb211@uga.edu>
2019-09-28 13:34:48 -0400

    Fixed indentation and added a Shape array to Driver.java to save students some time

--- src/cs1302/ce14/Driver.java
+++ src/cs1302/ce14/Driver.java
@@ -8,7 +8,12 @@ import java.util.Arrays;
 public class Driver {
 
     public static void main(String[] args) {
-    
+        Shape[] s = new Shape[] {
+            new Circle(4.5),
+            new Ellipse(3.2, 2.8),
+            new Circle(2.3)
+        };
+
     } // main
 
 } // Driver
